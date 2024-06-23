02bf117c823b70b582ae52bca1b4e0d3e0986e00
45Eva <126328952+45Eva@users.noreply.github.com>
2024-06-23 04:22:12 +0300

    індекс відповідності

--- main.py
+++ main.py
@@ -31,6 +31,15 @@ def vigenere_encrypt(plaintext, key):
     return ''.join(ciphertext)
 
 
+# Функція для обчислення індексу відповідності
+def calculate_index_of_coincidence(text):
+    n = len(text)
+    frequencies = {char: text.count(char) for char in ALPHABET}
+    index_of_coincidence = sum(f * (f - 1) for f in frequencies.values())
+    index_of_coincidence /= n * (n - 1) if n > 1 else 1
+    return index_of_coincidence
+
+
 # Функція для читання тексту з файлу
 def read_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
@@ -58,7 +67,9 @@ def main():
 
         for key_length, key in KEYS.items():
             encrypted_text = vigenere_encrypt(plaintext, key)
+            index_of_coincidence = calculate_index_of_coincidence(encrypted_text)
             result.append(f"Ключ {key_length} ({key}):\n{encrypted_text}\n")
+            print(f"На ключі довжини {key_length} ({key}) індекс відповідності між ВТ та ШТ: {index_of_coincidence:.6f}")
 
         write_file("Ш.txt", '\n'.join(result))
         print("Текст зашифровано. Результат записано у файл Ш.txt.")
